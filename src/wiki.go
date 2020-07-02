233d2cae769204894a5d079d05f7df60f8972f0b
Isaac Bordas <isaac.bordas@gmail.com>
2020-07-02 11:26:50 +0200

    Closure

--- src/wiki.go
+++ src/wiki.go
@@ -40,11 +40,7 @@ func loadPage(title string) (*Page, error) {
 	return &Page{Title: title, Body: body}, nil
 }
 
-func viewHandler(w http.ResponseWriter, r *http.Request)  {
-	title, err := getTitle(w, r)
-	if err != nil {
-		return
-	}
+func viewHandler(w http.ResponseWriter, r *http.Request, title string)  {
 	p, err := loadPage(title)
 	if err != nil {
 		http.ServeFile(w, r, PagePath + "NotFound")
@@ -52,11 +48,7 @@ func viewHandler(w http.ResponseWriter, r *http.Request)  {
 	renderTemplate(w, "view", p)
 }
 
-func editHandler(w http.ResponseWriter, r *http.Request) {
-	title, err := getTitle(w, r)
-	if err != nil {
-		return
-	}
+func editHandler(w http.ResponseWriter, r *http.Request, title string) {
 	p, err := loadPage(title)
 	if err != nil {
 		p = &Page{Title: title}
@@ -64,14 +56,10 @@ func editHandler(w http.ResponseWriter, r *http.Request) {
 	renderTemplate(w, "edit", p)
 }
 
-func saveHandler(w http.ResponseWriter, r *http.Request) {
-	title, err := getTitle(w, r)
-	if err != nil {
-		return
-	}
+func saveHandler(w http.ResponseWriter, r *http.Request, title string) {
 	body := r.FormValue("body")
 	p := &Page{Title: title, Body: []byte(body)}
-	err = p.save()
+	err := p.save()
 	if err != nil {
 		sendHttpError(w, err)
 		return
@@ -79,8 +67,19 @@ func saveHandler(w http.ResponseWriter, r *http.Request) {
 	http.Redirect(w, r, PageViewRoute + title, http.StatusFound)
 }
 
+func makeHandler(fn func(http.ResponseWriter, *http.Request, string)) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		m := validPath.FindStringSubmatch(r.URL.Path)
+		if m == nil {
+			http.NotFound(w, r)
+			return
+		}
+		fn(w, r, m[2])
+	}
+}
+
 func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
-	err := templates.ExecuteTemplate(w, PagePath + tmpl + ".html", p)
+	err := templates.ExecuteTemplate(w, tmpl + ".html", p)
 	if err != nil {
 		sendHttpError(w, err)
 		return
@@ -106,8 +105,8 @@ func getWorkingDir() string {
 }
 
 func main()  {
-	http.HandleFunc(PageViewRoute, viewHandler)
-	http.HandleFunc(PageEditRoute, editHandler)
-	http.HandleFunc(PageSaveRoute, saveHandler)
+	http.HandleFunc(PageViewRoute, makeHandler(viewHandler))
+	http.HandleFunc(PageEditRoute, makeHandler(editHandler))
+	http.HandleFunc(PageSaveRoute, makeHandler(saveHandler))
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
